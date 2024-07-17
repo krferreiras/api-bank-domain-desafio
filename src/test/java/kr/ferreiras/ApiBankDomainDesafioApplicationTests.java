e50cf844c8d3e227a333022edb39bd55f6d54933
krferreiras <dossantos.krf@gmail.com>
2024-07-17 18:26:30 -0300

    add project

---
+++ src/test/java/kr/ferreiras/ApiBankDomainDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ferreiras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBankDomainDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
